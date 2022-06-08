536075bbae351253c969c14ffe1c99252a8be30c
MICHAEL FRANK LANGDON <mikes4bikes@gmail.com>
2022-06-08 10:18:37 -0500

    Updates to the typing animator.

--- typing/js/typeset.js
+++ typing/js/typeset.js
@@ -1,69 +1,51 @@
-	$(document).ready(function(){	
-		var mySound = new buzz.sound( "typewriter.mp3");
-		mySound.load(); 
-		mySound.loop().play();
-		
-		var ndRtrnSnd = new buzz.sound ("carriagereturn.mp3");
-		ndRtrnSnd.load();
-		
-		$( '#typers' ).click( function() {
-		mySound.mute(); 
-		ndRtrnSnd.mute();
+$(function(){
+	let mySound = new buzz.sound("assets/keyboard.mp3", {
+		preload: true,
+		autoplay: false,
+		loop: true
 	});
-		 
+
+	let ndRtrnSnd = new buzz.sound("assets/carriagereturn.mp3", {
+		preload: true,
+		autoplay: false,
+		loop: false
+	}); 
+
+	$( '#typers' ).dblclick( function() { //mutes the sound if you click on the page
+		mySound.toggleMute();
+		ndRtrnSnd.toggleMute();
 	
-	mySound.loop()
-    .bind( "timeupdate", function() {
-       var timer = buzz.toTimer( this.getTime() );
-       document.getElementById( "timer" ).innerHTML = timer; 
-    });
-	  
+	});
+
+	$( '#typeNow' ).click( function() { //mutes the sound if you click on the page
+		$(".enter-type").css("visibility","hidden");
+		$(".typed-type ").css("visibility","visible");
+		var pauseTime = "^" + String($("#pause").val() * 1000) + "\n";
+		console.log(pauseTime);
+		var typeSpeedTime = Number($("#typespeed-id").val());
+		console.log(typeSpeedTime);
+		var docCode = String($("#enter-type-id-textarea").val());
+		docCode = docCode.replace(/\n/g, pauseTime);
+		console.log(docCode);
+			$("#typed").typed({
+				strings: [docCode],
+				contentType: 'text',
+				typeSpeed: typeSpeedTime,
+				loop: false,
+				loopCount: false,
+				callback: function(){
+					mySound.stop(); //stops the typewriter when it encounters a carat.
+					ndRtrnSnd.play();  //plays the carriage return
+				},
+				restartPlay: function() {
+					ndRtrnSnd.stop(); //stops the carriage return
+					mySound.play(); //begins typing again
+				},
+				resetCallback: function() {
+					mySound.stop();  //stops everything when complete
+					ndRtrnSnd.stop(); //stops everything when complete
+				}
+			})
+	})
 	
-	 
-		$("#typed").typed({
-			strings: ["<strong>LearnEngineer.org</strong> ^1000\nWe engineer learning for efficiency.^1000\n^1000\nLearningEngineer.org is a non-profit...^1000 \ndevoted to creating materials...^1000 \nthat promote...^1000 \nscience...^1000 \nreason...^1000 \nand learning.^1000\n^1000 \nLinks:^1000\n<a href='http://google.com'>Google.com</a>"],
-			contentType: 'html',
-			typeSpeed: 100,
-            loop: false,
-            loopCount: false, 
-			callback: function(){
-				mySound.stop();
-				ndRtrnSnd.play(); 
-			},
-			restartPlay: function() {
-			    ndRtrnSnd.stop(); 
-				mySound.play();
-			},
-			resetCallback: function() {
-				mySound.stop();
-				ndRtrnSnd.stop();
-			}
-			
-		}); 
-
-
-function shift(){
-    mySound.play();
-}
-
-function terminalHeight(){
-  var termHeight = $(".terminal-height");
-  var value = termHeight.text();
-  value = parseInt(value);
-  setTimeout(function(){
-      if (value > 40){
-          value = value-1;
-          this.txtValue = value.toString();
-          termHeight.text(this.txtValue);
-          self.terminalHeight();
-          
-      }
-      else{
-          alert('stop');
-          clearTimeout();
-      }
-  }, 40);}
-})
-
-
- 
+});
